ef25d42db2eb9043d907f0c0112c7d55cf85d399
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-11-15 12:27:37 -1000

    includes node environment variables

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
+const PORT = require(`./config/${process.env.NODE_ENV}`).PORT
 
 const counter = {
   count: 0
